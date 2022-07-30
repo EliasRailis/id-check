3169a712ce2aec480b269e56d47a43aaf9ca3232
EliRailis <elias.railis97@gmail.com>
2022-07-31 01:01:59 +0200

    ID used for testing

--- src/IdCheck.Console/Program.cs
+++ src/IdCheck.Console/Program.cs
@@ -2,6 +2,7 @@
 using IdCheck;
 using IdCheck.Enums;
 
+// This ID is used only for testing purposes
 var validate = Id.Validate("8605065397083");
 var validateWithDetailedOption = Id.Validate("8605065397083", Options.Detailed);
 
